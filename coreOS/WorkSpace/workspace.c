4bc9894123c30987aee92921a4614a19ac714301
GetRevanced <136936377+GetRevanced@users.noreply.github.com>
2023-10-12 17:35:28 +0300

    Error message Window builder
    Error message Window builder which you can use to generate error pop ups system-wide in the operating system.

--- coreOS/WorkSpace/workspace.c
+++ coreOS/WorkSpace/workspace.c
@@ -20,3 +20,24 @@ kWorkSpaceFunc workspaceButton_Constructor(int begin_x, int begin_y, int end_x, 
     micrOS_PaintRectangle(begin_x, begin_y, end_x, end_y, buttonColor, 1);
     micrOS_WriteLine(begin_x + 5, begin_y + 5, buttonText, 0x0f, buttonColor, 1);
 }
+
+kWorkSpaceFunc createErrorWindow(char* errorMessage) {
+    int windowWidth = 400;
+    int windowHeight = 200;
+    int windowX = (1920 - windowWidth) / 2;
+    int windowY = (1080 - windowHeight) / 2; 
+    unsigned char windowColor = 0x19;
+    char* windowTitle = "Error";
+    bool isDismissable = true;
+    workspaceWindow_Constructor(windowX, windowY, windowX + windowWidth, windowY + windowHeight, windowColor, windowTitle, isDismissable);
+
+    int textX = windowX + 20;
+    int textY = windowY + 50;
+    micrOS_WriteLine(textX, textY, errorMessage, 0x0f, windowColor, 1);
+    int buttonX = windowX + windowWidth / 2 - 50;
+    int buttonY = windowY + windowHeight - 40;
+    unsigned char buttonColor = 0x0f;
+    char* buttonText = "OK";
+    workspaceButton_Constructor(buttonX, buttonY, buttonX + 100, buttonY + 30, buttonColor, buttonText);
+    return WorkSpace_OK;
+}
